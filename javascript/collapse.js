305d081437f759cf70cd5a55cadc825e3e300431
dschadlich <dschadlich@gmail.com>
2016-03-16 16:16:50 -0400

    Made some changes to the way your colapse feature works: First I removed your event listeners out of the html (some of them, there are some more for shutter). Then I registered them to the approprate objects in the collapse.js file I reworked your code so that it does not relay on the id of the div, I look at click events, and parents of elemets that recieved the event. The goal is to move away from ids and towards classes, also with this method you can easily add new sections of the site with out worrying about having the id numbers match. I reorganized some of the HTML structure to make it work better with the JS code, mainly I moved the div with the isUp/isDown class into the div with the main and solid class. This commit is mostly about getting the function of the JS away from the ids, and the code has some uneeded lines, as well as alot of debug code present within it.

--- javascript/collapse.js
+++ javascript/collapse.js
@@ -3,28 +3,50 @@
  */
 
 /*
- * 
+ *
  * target element has to contain class value "isDown" or "isUp"
- * 
+ *
+ document.getElementById("someButton").onclick = function() {
+   alert("Hello");
+ };
  */
+
+ window.onload = function () {
+   setInitialCollapseState ();
+
+ };
 
 function setInitialCollapseState() {
     /*
-     * 
+     *
      * called by onload attribute of body tag in html
-     * 
+     *
      */
     var controlNodeList = document.getElementsByClassName('collapseControl');
     var targetElem;
     var img;
     var txt;
-    
+console.log (controlNodeList);
     for(var i=0;i<controlNodeList.length;i++){
         targetElem = getTarget(controlNodeList[i]);
-        
+        console.log (targetElem);
+
+        controlNodeList[i].onclick = function (objectSrc){
+          console.log ("got click");
+
+          toggleCollapse (objectSrc.target);
+        }
+
+/*
+          function (){
+            console.log
+            toggleCollapse()
+          }; //not sure if I should use this in this case
+*/
+
         img = controlNodeList[i].children[1];
         txt = controlNodeList[i].children[0];
-        
+
         if (isDown(targetElem)) {
             txt.innerHTML = "Hide";
             img.src = "img/arrowDown.png";
@@ -37,13 +59,19 @@ function setInitialCollapseState() {
     }
 }
 
-function toggleCollapse(control) {  
-    
-    var img = control.children[1];
-    var txt = control.children[0];
+function toggleCollapse(control) {
+    console.log ("toggleCollapse");
+    console.log (control);
+    //console.log (control.parentElement);
+
+    var img = control.parentElement.children[1];
+    var txt = control.parentElement.children[0];
 
     var targetElem = getTarget(control);
-        
+    console.log (targetElem.className);
+
+
+
     if (isDown(targetElem)) {
         txt.innerHTML = "Show";
         img.src = "img/arrowLeft.png";
@@ -55,11 +83,15 @@ function toggleCollapse(control) {
         targetElem.style.display = "block";
         targetElem.className = targetElem.getAttribute("class").replace("isUp", "isDown");
     }
+
+
 }
 
 function isDown(targetElem) {
+  console.log ("isDown?");
+  console.log (targetElem);
     var targetElemClasses = targetElem.getAttribute("class");
-    
+
     if(targetElemClasses.indexOf("isDown") > -1) {
         return true;
     } else {
@@ -68,16 +100,21 @@ function isDown(targetElem) {
 }
 
 function getTarget(control) {
+  console.log ("getTarget");
+  //console.log (control);
+  console.log (control.parentElement.parentElement);
+  return control.parentElement.parentElement.getElementsByClassName ("controlsTarget")[0];
+
     /*
-     * 
-     * controlIdString has the purpose of identifying the target. the following 
-     * substring and concatenation statement convert the button's id into the 
-     * id of the element it targets 
-     * 
+     *
+     * controlIdString has the purpose of identifying the target. the following
+     * substring and concatenation statement convert the button's id into the
+     * id of the element it targets
+     *
      */
     var controlIdString = control.getAttribute("id");
     var targetIdString = "t" + controlIdString.substring(9);
-    var targetElem = document.getElementById(targetIdString); 
-    
+    var targetElem = document.getElementById(targetIdString);
+
     return targetElem;
 }
